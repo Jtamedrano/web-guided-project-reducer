6ee9da1fbcbce37288686c01f7b25f1dd503c329
Jesse Medrano <Jtamedrano@gmail.com>
2021-01-26 20:11:08 -0800

    made an actual calculator

--- review/src/App.js
+++ review/src/App.js
@@ -1,15 +1,78 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from 'react';
 import reducer from './reducers/calcReducer';
-import "./styles.css";
+import './styles.css';
 
+const initialState = {
+  inputValue: 0,
+  currentValue: 0,
+  memory: 0,
+  lastAction: '',
+};
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function handleBtnClick(e, btn) {
+    e.preventDefault();
+    switch (btn) {
+      case 'ADD':
+        dispatch({ type: 'ADD', payload: state.inputValue });
+        dispatch({ type: 'EQUALS' });
+        break;
+      case 'SUBTRACT':
+        dispatch({ type: 'SUBTRACT', payload: state.inputValue });
+        dispatch({ type: 'EQUALS' });
+        break;
+      case 'EQUALS':
+        dispatch({ type: state.lastAction, payload: state.inputValue });
+        dispatch({ type: 'EQUALS' });
+        break;
+    }
+  }
+  console.log(state);
   return (
     <div className="App">
-      <textarea rows="1" value="0" id="total" type="text" name="ans"></textarea>
+      <textarea
+        rows="1"
+        value={state.inputValue} // sets the value to the inputValue from reducer
+        onFocus={(e) => {
+          e.preventDefault();
+          dispatch({ type: 'TYPE_INPUT', payload: '' });
+        }} // clears input so user can type freely
+        onChange={(e) =>
+          dispatch({ type: 'TYPE_INPUT', payload: e.target.value })
+        } // acts as setValue
+        id="total"
+        type="text"
+        name="ans"
+      ></textarea>
       <br />
-      <button type="button" className="btn">
+      <button
+        type="button"
+        className="btn"
+        onClick={(e) => {
+          handleBtnClick(e, 'ADD');
+        }}
+      >
         +
+      </button>
+      <button
+        type="button"
+        className="btn"
+        onClick={(e) => {
+          handleBtnClick(e, 'SUBTRACT');
+        }}
+      >
+        -
+      </button>
+      <button
+        type="button"
+        className="btn"
+        onClick={(e) => {
+          handleBtnClick(e, 'EQUALS');
+        }}
+      >
+        =
       </button>
     </div>
   );
--- review/src/reducers/calcReducer.js
+++ review/src/reducers/calcReducer.js
@@ -1,17 +1,28 @@
 const reducer = (state, action) => {
-    switch (action.type) {
-      case "ADD":
-        return { ...state, currentValue: state.currentValue + action.payload };
-      case "SUBTRACT":
-        return { ...state, currentValue: state.currentValue - action.payload };
-      case "CLEAR":
-        return { ...state, currentValue: 0 };
-      case "ADD_TO_MEMORY":
-        return { ...state, memory: state.currentValue };
-      default:
-        return state;
-    }
-  };
-  
-  export default reducer;
-  
+  switch (action.type) {
+    case 'TYPE_INPUT':
+      return { ...state, inputValue: `${action.payload}` };
+    case 'ADD':
+      return {
+        ...state,
+        currentValue: state.currentValue + parseInt(action.payload),
+        lastAction: action.type,
+      };
+    case 'SUBTRACT':
+      return {
+        ...state,
+        currentValue: state.currentValue - parseInt(action.payload),
+        lastAction: action.type,
+      };
+    case 'CLEAR':
+      return { ...state, currentValue: 0 };
+    case 'ADD_TO_MEMORY':
+      return { ...state, memory: state.currentValue };
+    case 'EQUALS':
+      return { ...state, inputValue: state.currentValue };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
